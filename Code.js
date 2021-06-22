156074e0c2ca718597a121f33a414ff0857ebcff
meher745 <75887880+meher745@users.noreply.github.com>
2021-06-22 17:35:16 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("mistyrose")
+    text("Hint: Always changing, not constant", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white");
+    text("ATADASBE",400,190);
+    fill("mistyrose");
+    text("Hint: Stores all information",400,210);
+    fill("white");
+    text("CUTNIFON",700,300);
+    fill("mistyrose");
+    text("Hint: Performs a particular task",700,320);
+
+}
